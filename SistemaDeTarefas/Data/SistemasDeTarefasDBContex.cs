5e608543b3eb0678a9fc72baf0120ab6e71e6c1e
Cárita Martins <caritadm@hotmail.com>
2024-12-06 11:44:36 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaDeTarefas/Data/SistemasDeTarefasDBContex.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefas.Data.Map;
+using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Data
+{
+    public class SistemasDeTarefasDBContex : DbContext
+    {
+        //Trabalhando com ORM? URM? ele facilita trabalhar com banco de dados
+        public SistemasDeTarefasDBContex(DbContextOptions<SistemasDeTarefasDBContex> options)
+            :base(options)
+        {
+        }
+
+        //Representa uma tabela no banco de dados Usuarios
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+        public DbSet<TarefaModel> Tarefas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UsuarioMap());
+            modelBuilder.ApplyConfiguration(new TarefaMap());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
